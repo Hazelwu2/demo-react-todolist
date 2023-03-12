aabfc3e118d246546c21db2a4c0c3c1765c3815f
Hazel Wu <tinazx056@gmail.com>
2023-03-12 18:37:53 +0800

    feat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
 import axios from 'axios'
 import { useState } from 'react'
+import { BiMessageSquareAdd } from 'react-icons/bi';
+import { MdDeleteOutline } from 'react-icons/md';
+
 
 function App() {
   const [input, setInput] = useState('')
-  const [todoList, setTodoList] = useState([])
+  const [todos, setTodos] = useState([
+    { id: 0, title: '爆肝趕 KryptoCamp 作業', status: false },
+    { id: 1, title: '觀看 KryptoCamp 線上課程', status: false },
+    { id: 2, title: '學習 Solidity 合約', status: false },
+  ])
   const [completedTodo, setCompletedTodo] = useState(0)
 
   const test = () => { }
+  const addTodo = (text) => {
+    setTodos([...todos, { title: text }])
+    setInput('')
+    alert('新增成功')
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  const makeCompletedTodo = (id) => {
+    // 深複製
+    const newTodos = [...todos];
+    newTodos[id].status = !newTodos[id].status
+    setTodos(newTodos)
+
+    console.log(newTodos[id])
+  }
+
 
   return (
     <div className='App'>
@@ -20,63 +46,77 @@ function App() {
           </header>
 
           <div class='todolist__form'>
-            <input id='todo' type='text' placeholder='新增待辦事項' />
-            <div>+</div>
-            {/* <a>
-              <img src='./images/add.svg' alt='Create Todo'>
-              Add
-            </a> */}
+            <input
+              id='todo'
+              type='text'
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value)
+              }}
+              placeholder='新增待辦事項'
+            />
+
+            <div class="add-todo-btn" onClick={() => addTodo(input)}>
+              <BiMessageSquareAdd />
+            </div>
           </div>
 
 
           <div class='todolist'>
             <ul class='todolist__tabs'>
+
               <li>
-                <a onClick={() => test}
+                <span onClick={() => test}
                   class='all active'
                   href='#'>
                   全部
-                </a>
+                </span>
               </li>
               <li>
-                <a onClick={() => { }}
+                <span
                   class='no-completed' href='#'>
                   待完成
-                </a>
+                </span>
               </li>
               <li>
-                <a onClick={() => { }}
-                  class='completed' href='#'>已完成
-                </a>
+                <span
+                  class='completed' href='#'>
+                  已完成
+                </span>
               </li>
             </ul>
 
             <div class='todolist__table'>
+
+              {todos && todos?.length === 0 && (
+                <div>無待辦事項</div>
+              )}
+
               <ul class='todolist__item'>
-                <li class=' no-completed'>
-                  <input class='todolist__input' type='checkbox' />
-                  <span>爆肝趕 KryptoCamp 作業</span>
-                  <a class='delete' href='#'>
-                    <i class='fa fa-x'></i>
-                  </a>
-                </li>
+                {todos && todos?.map((todo, i) => {
+                  return (
+                    <li
+                      key={i}
+                      class={todo?.status ? 'completed' : 'no-completed'}
+                    >
 
+                      <input
+                        class='todolist__input'
+                        type='checkbox'
+                        id={`todo-${i}`}
+                        onChange={() => makeCompletedTodo(i)}
+                      />
+                      <label htmlFor={`todo-${i}`}>{todo?.title}</label>
 
-                <li class='no-completed'>
-                  <input class='todolist__input' type='checkbox' />
-                  <span>觀看 KryptoCamp 線上課程</span>
-                  <a class='delete' href='#'>
-                    <i class='fa fa-x'></i>
-                  </a>
-                </li>
-
-                <li class='no-completed'>
-                  <input class='todolist__input' type='checkbox' />
-                  <span>學習 Solidity 合約</span>
-                  <a class='delete' href='#'>
-                    <i class='fa fa-x'></i>
-                  </a>
-                </li>
+                      <button
+                        onClick={() => deleteTodo(i)}
+                        class='delete'
+                      >
+                        <MdDeleteOutline />
+                      </button>
+                    </li>
+                  )
+                })}
               </ul>
 
               <div class='todolist__info'>
@@ -84,7 +124,7 @@ function App() {
                   <a>{completedTodo}</a>
                   個已完成項目
                 </span>
-                <span class='cursor-pointer' onclick='clearCompletedTodo()'>清除已完成項目</span>
+                <span class='cursor-pointer' onClick='clearCompletedTodo()'>清除已完成項目</span>
               </div>
             </div>
           </div>
